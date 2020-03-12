9fc7d145ac03ee067f4788e190f42d10935a2c4e
Aizelmarie M <amagsino@users.noreply.github.com>
2020-03-12 17:58:09 -0400

    edit

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// Function for change on dropdown menu
+function optionChanged(selectedID){
+
+    // Check if value is selected in dropdown
+    console.log(selectedID);
+ 
+    // Read the json file for the data
+    d3.json("data/merged_results.json").then((data) => {
+ 
+    console.log(data);
